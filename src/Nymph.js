85a2a902bddfb202932e1acc0904bc18a1cec910
Hunter Perrin <hperrin@gmail.com>
2018-08-01 16:38:32 -0700

    Version bump.

--- src/Nymph.js
+++ src/Nymph.js
@@ -599,7 +599,7 @@ export class Nymph {
 // === Static Properties ===
 
 // The current version of Nymph Client.
-Nymph.version = '4.0.0-beta.12';
+Nymph.version = '4.0.0-beta.13';
 Nymph.entityClasses = {};
 Nymph.responseCallbacks = [];
 
